72d73198ceb0c03b1b8be34105bcda098e0b5b7b
AndrewB4y <andresbaymon@gmail.com>
2018-09-21 10:47:19 -0500

    Algo nuevo

--- PV_Carga_U/PV_Carga_U.ino
+++ PV_Carga_U/PV_Carga_U.ino
@@ -14,13 +14,18 @@
 int incomingByte = 0;
 String inString = "";
 String an = " ";
-int vl = 0;
+int i1 = 0, i2 = 0;
+//int i3 = 0, vl = 0;
 
-char aa, tramRasp[9];
-String a, si3, ssoc;
-int i3, soc;
-char frst, scnd;
-bool v = false;
+String a, spm, sqm, svl, ssoc;
+//String si3;
+int pm, qm, vl, soc;
+//int i3;
+char frst, scnd, thrd, frth;
+
+char aa, tramRasp[17];
+bool bi1 = false, bi2 = false;
+//bool v = false, bi3 = false;
 
 void setup()
 {
@@ -31,13 +36,21 @@ void setup()
   //Can0.begin(1000000);
   Can0.begin(CAN_BPS_250K);
   //set the filters for each mailbox and its respective mask
-  Can0.setRXFilter(0, 0x03, 0xFF, false);
-  Can0.setCallback(0, mensaje0);
+  Can0.setRXFilter(0, 0x06, 0xFF, false);
+  Can0.setRXFilter(1, 0x07, 0xFF, false);
+//  Can0.setRXFilter(2, 0x08, 0xFF, false);
+//  Can0.setRXFilter(3, 0x03, 0xFF, false);
 
+  Can0.setCallback(0, mensaje0);
+  Can0.setCallback(1, mensaje1);
+//  Can0.setCallback(2, mensaje2);
+//  // Callback para recibir VL
+//  Can0.setCallback(3, mensaje3);
   // Can0.watchFor();
   // Set the pins to control the steppers
 
 }
+
 
 void SendData(int ident, int a )
 {
@@ -52,29 +65,41 @@ void SendData(int ident, int a )
 }
 
 void mensaje0(CAN_FRAME *frame) {
-
-  setvl(frame);
-
+  seti1(frame);
 }
 
-void setvl(CAN_FRAME *frame) {
 
-  vl = int(frame->data.low);
-  Serial.println(vl);
-  v = true;
+void mensaje1(CAN_FRAME *frame) {
+  seti2(frame);
+}
+
+void seti1(CAN_FRAME *frame) {
+
+  i1 = int(frame->data.low);
+  //Serial.println(i1);
+  bi1 = true;
+  sendToRasp();
+}
+
+void seti2(CAN_FRAME *frame) {
+
+  i2 = int(frame->data.low);
+  bi2 = true;
   sendToRasp();
 }
 
 void sendToRasp() {
-  if (v) {
-    sprintf(tramRasp, "v%07de", vl);
+  if (bi1 && bi2) {
+    // biomasa, diesel, vl
+    sprintf(tramRasp, "b%07dd%07de", i1, i2);
     Serial.print(tramRasp);
-    v = false;
+    bi1 = false;
+    bi2 = false;
   }
 }
 
-
 void receiveRaspData() {
+
   while (Serial.available() == 0) {}
   a = "";
   while (Serial.available() > 0) {
@@ -83,20 +108,27 @@ void receiveRaspData() {
     if (aa == '\n') {
       frst = a.charAt(0);
       scnd = a.charAt(8);
+      thrd = a.charAt(16);
+      frth = a.charAt(24);
       //Serial.println(head);
-      if (frst == 'i') {
-        si3 = a.substring(1, 8);
-        i3 = si3.toInt();
-        ssoc = a.substring(9, 15);
+      if (frst == 'p') {
+        spm = a.substring(1, 8);
+        pm = spm.toInt();
+        sqm = a.substring(9, 16);
+        qm = sqm.toInt();
+        svl = a.substring(17, 24);
+        vl = svl.toInt();
+        ssoc = a.substring(25, 31);
         soc = ssoc.toInt();
       }
     }
   }
 }
 
-
 void loop() {
   receiveRaspData();
-  SendData(0x08, i3);
+  SendData(0x01, pm);
+  SendData(0x02, qm);
+  SendData(0x04, vl);
   SendData(0x0B, soc);
 }
